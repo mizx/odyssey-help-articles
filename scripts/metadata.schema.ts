0492d38ec451c0453e6990635c342891da27be1e
Cody Mathisen <cody.mathisen@epicgames.com>
2024-01-04 17:31:27 -0600

    chore: update schemas to use key-based models

--- scripts/metadata.schema.ts
+++ scripts/metadata.schema.ts
@@ -1,22 +1,22 @@
 import z from "zod";
 
 const modelSchema = z.enum([
-  "2024 Beast",
-  "2024 Mystic",
-  "2024 Stealth",
-  "2024 Classic",
-  "2023 Beast",
-  "2023 Stealth",
-  "2023 Classic",
-  "2023 LT",
-  "2022 Beast",
-  "2022 Stealth",
-  "2022 Classic",
-  "2022 LT",
-  "2021 Beast",
-  "2021 Stealth",
-  "2021 Classic",
-  "2020 MODE",
+  "2024-beast",
+  "2024-mystic",
+  "2024-stealth",
+  "2024-classic",
+  "2023-beast",
+  "2023-stealth",
+  "2023-classic",
+  "2023-lt",
+  "2022-beast",
+  "2022-stealth",
+  "2022-classic",
+  "2022-lt",
+  "2021-beast",
+  "2021-stealth",
+  "2021-classic",
+  "2020-mode",
 ]);
 
 const tagSchema = z.enum([
